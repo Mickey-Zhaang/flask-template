9a9ab897e6a99c457ce3f390013e7268cf6f1ff3
Mickey_zhaang <qzhang27@colby.edu>
2025-08-10 15:28:42 -0700

    Initialized logic to-be-tested

---
+++ website/math.py
@@ -0,0 +1,27 @@
+"""
+math.py: dedicated to performing basic arithmetic operations
+"""
+
+
+def add(x: int, y: int) -> int:
+    """
+    Adds two numbers
+    params:
+        x: int
+        y: int
+    returns:
+        sum: int
+    """
+    return x + y
+
+
+def subtract(x: int, y: int) -> int:
+    """
+    Subtracts two numbers
+    params:
+        x: int
+        y: int
+    returns:
+        difference: int
+    """
+    return x + y
